0063ddd2b108aaf0411aaf0a811c87dfd36e8669
Bobby R. Bruce <bobbybruce1990@gmail.com>
2018-10-08 14:45:17 -0700

    Added IClassCollapser

---
+++ reachability-analysis/src/main/java/edu/ucla/cs/onr/classcollapser/IClassCollapser.java
@@ -0,0 +1,27 @@
+package edu.ucla.cs.onr.classcollapser;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+public interface IClassCollapser {
+
+	/**
+	 * The purpose of a 'collapseClasses' method is to delete any unused classes and collapse what remains to the
+	 * minimum possible. The goal is to reduce the size of the bytecode while preserving overall semantics.
+	 *
+	 * @param libClassPath The classpath of any libraries needed for the compilation of the app
+	 * @param appClassPath The classpath of the app
+	 * @param touchedMethods The touched methods. We conform to the following schema:
+	 *                        '[qualified class name]:[method]'
+	 *                        or '[qualified class name]:*', when declaring all methods in a class
+	 *                        E.g., 'edu.ucla.cs.onr.methodwiper.MethodWiper:wipeMethodStart'
+	 *
+	 *                       Note: We do not support method overloading yet. Two methods, with the same name,
+	 *                       will be treated as the same, even if their signatures differ. E.g., we can not distinguish
+	 *                       between 'wipeMethodStart()' and 'wipeMethodStart(int)'. If 'wipeMethodStart()' were
+	 *                       touched, please consider 'wipeMethodStart(int) touched also.
+	 *
+	 */
+	public void collapseClasses(List<File> libClassPath, List<File> appClassPath, Set<String> touchedMethods);
+}
