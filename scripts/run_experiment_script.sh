2e7174fa641527257b6e9a2ab5fb9dfb9a00c549
Bobby R. Bruce <b.bruce@cs.ucla.edu>
2019-03-20 18:47:35 -0700

    Added Tamiflex to experiment script.

--- scripts/run_experiment_script.sh
+++ scripts/run_experiment_script.sh
@@ -35,7 +35,7 @@ cat ${WORK_LIST} |  while read item; do
 	temp_file=$(mktemp /tmp/XXXX)
 
 	#A two hour timeout
-	timeout 7200 ${JAVA} -Xmx20g -jar ${DEBLOAT_APP} --maven-project ${item_dir} -T --public-entry --main-entry --test-entry --prune-app --remove-methods --verbose 2>&1 >${temp_file} 
+	timeout 7200 ${JAVA} -Xmx20g -jar ${DEBLOAT_APP} --tamiflex poa-2.0.3.jar --maven-project ${item_dir} -T --public-entry --main-entry --test-entry --prune-app --remove-methods --verbose 2>&1 >${temp_file} 
 	exit_status=$?
 	if [[ ${exit_status} == 0 ]]; then
 		cat ${temp_file}
