a162018e7be4cf8059a17e3f3fff3c0ad50851ef
Bobby R. Bruce <b.bruce@cs.ucla.edu>
2019-06-10 09:25:39 -0700

    Fixed bug where the number of app/lib methods removed was not being calculated correctly.

--- reachability-analysis/jshrink-app/src/main/java/edu/ucla/cs/jshrinkapp/Application.java
+++ reachability-analysis/jshrink-app/src/main/java/edu/ucla/cs/jshrinkapp/Application.java
@@ -240,7 +240,7 @@ public class Application {
 
 				// wipe the body of virtually invoked methods, keep their method headers
 				appMethodsRemoved.addAll(jShrink.wipeMethods(appVirtualMethodsToWipe));
-				appMethodsRemoved.addAll(jShrink.wipeMethods(libVirtualMethodsToWipe));
+				libMethodsRemoved.addAll(jShrink.wipeMethods(libVirtualMethodsToWipe));
 
 				removedMethod = true;
 			} else if (commandLineParser.includeException()) {
@@ -251,7 +251,7 @@ public class Application {
 
 				appMethodsRemoved.addAll(jShrink.wipeMethodAndAddException(appVirtualMethodsToWipe,
 						commandLineParser.getExceptionMessage()));
-				appMethodsRemoved.addAll(jShrink.wipeMethodAndAddException(libVirtualMethodsToWipe,
+				libMethodsRemoved.addAll(jShrink.wipeMethodAndAddException(libVirtualMethodsToWipe,
 						commandLineParser.getExceptionMessage()));
 
 				if (commandLineParser.getExceptionMessage().isPresent()) {
@@ -264,7 +264,7 @@ public class Application {
 				libMethodsRemoved.addAll(jShrink.wipeMethods(libMethodsToRemove));
 
 				appMethodsRemoved.addAll(jShrink.wipeMethods(appVirtualMethodsToWipe));
-				appMethodsRemoved.addAll(jShrink.wipeMethods(libVirtualMethodsToWipe));
+				libMethodsRemoved.addAll(jShrink.wipeMethods(libVirtualMethodsToWipe));
 
 				wipedMethodBody = true;
 			}
