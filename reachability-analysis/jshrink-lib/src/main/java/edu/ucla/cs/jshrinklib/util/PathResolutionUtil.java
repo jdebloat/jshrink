60afbb7463e63c68bf8a43af359713b81bebe8d7
jay-ucla <jasa92@g.ucla.edu>
2019-08-07 22:02:30 -0700

    Fixed Class collapser bug - dependency graph created at end with modified classes:

---
+++ reachability-analysis/jshrink-lib/src/main/java/edu/ucla/cs/jshrinklib/util/PathResolutionUtil.java
@@ -0,0 +1,51 @@
+package edu.ucla.cs.jshrinklib.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+
+public class PathResolutionUtil {
+	public static HashMap<String, String> classPathMap = new HashMap<String, String>();
+	public static void buildMap(Set<File> class_paths){
+		for(File cp:class_paths)
+			PathResolutionUtil.readClass(cp);
+	}
+
+	public static void readClass(File dir){
+		try {
+			if (dir.isDirectory()) {
+				readClassFromDirectory(dir, "");
+			}
+			else {
+				throw new IOException("Cannot read classes from '" + dir.getAbsolutePath() +
+						"'. It is neither a directory or a jar.");
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static void readClassFromDirectory(File dirPath, String prefix) {
+		if(prefix.startsWith("."))
+			prefix = prefix.substring(1);
+		if(!dirPath.exists()) {
+			// fix NPE due to non-existent file
+			System.err.println(dirPath.getAbsolutePath() + " does not exist.");
+			return;
+		}
+
+		for(File f : dirPath.listFiles()) {
+			if(f.isDirectory()) {
+				readClassFromDirectory(f, prefix+"."+dirPath.getName());
+			} else {
+				String fName = f.getName();
+				if(fName.endsWith(".class")) {
+					PathResolutionUtil.classPathMap.put(prefix.substring(prefix.indexOf(".")+1)+"."+
+							dirPath.getName()+"."+fName.substring(0,fName.length()-6),f.getAbsolutePath());
+				}
+			}
+		}
+	}
+}
