331e23800734fcc33b529de5faaac5dba7f75bb6
husttroy <troyathust@gmail.com>
2020-01-27 22:09:19 -0500

    Bundle classfiles in a maven project into a jar file.

--- jshrink/jshrink-app/src/main/java/edu/ucla/cs/proguard/ProGuardRunner.java
+++ jshrink/jshrink-app/src/main/java/edu/ucla/cs/proguard/ProGuardRunner.java
@@ -7,6 +7,8 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProGuardRunner {
     private final String proguardJar;
@@ -45,6 +47,86 @@ public class ProGuardRunner {
             System.out.println(output);
         }
         process.waitFor();
+    }
+
+    /* Note: ProcessBuilder does not work with wildcards. So do not give a classfile path like target/classes/*.class */
+    public boolean bundleClassFiles(String baseDir, List<String> classFiles, String outputJarPath, boolean updateJar) throws IOException, InterruptedException {
+        File f = new File(outputJarPath);
+
+        String[] cmd = new String[3 + classFiles.size()];
+        cmd[0] = "jar";
+        if(updateJar) {
+            cmd[1] = "uf";
+        } else {
+            if(f.exists()) {
+                f.delete();
+            }
+            cmd[1] = "cf";
+        }
+
+        cmd[2] = outputJarPath;
+        for(int i = 3; i < classFiles.size() + 3; i++) {
+            cmd[i] = classFiles.get(i-3);
+        }
+
+        ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+        processBuilder.directory(new File(baseDir));
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        InputStream stdout = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(stdout);
+        BufferedReader br = new BufferedReader(isr);
+
+        String output = null;
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+        int exitCode = process.waitFor();
+        return exitCode == 0;
+    }
+
+    public boolean bundleMavenProject(String projectPath, String outputJarPath) throws IOException, InterruptedException {
+        File outputJar = new File(outputJarPath);
+        if (outputJar.exists()) {
+            outputJar.delete();
+        }
+
+        boolean updateJar = false;
+        File dir = new File(projectPath);
+        // find all target classes and test classes folders
+        ArrayList<String> classFolders =  new ArrayList<String>();
+        ArrayList<String> testFolders =  new ArrayList<String>();
+        traverseFolders(dir, classFolders, testFolders);
+        for(String classFolder : classFolders) {
+            File f = new File(classFolder);
+            ArrayList<String> files = new ArrayList<String>();
+            for(File f2 : f.listFiles()) {
+                files.add(f2.getName());
+            }
+
+            boolean exitCode = bundleClassFiles(classFolder, files, outputJarPath, updateJar);
+
+            if(!exitCode) {
+                return false;
+            }
+            updateJar = true;
+        }
+
+        return true;
+    }
+
+    private void traverseFolders(File dir, List<String> classFolders, List<String> testFolders) {
+        for(File f : dir.listFiles()) {
+            if(dir.getName().equals("target")) {
+                if (f.isDirectory() && f.getName().equals("classes")) {
+                    classFolders.add(f.getAbsolutePath());
+                } else if (f.isDirectory() && f.getName().equals("test-classes")) {
+                    testFolders.add(f.getAbsolutePath());
+                }
+            } else if (f.isDirectory()) {
+                traverseFolders(f, classFolders, testFolders);
+            }
+        }
     }
 
     /* set dependenciesPath to an empty string if the input jar file contains all its dependencies */
--- jshrink/jshrink-app/src/test/java/edu/ucla/cs/jshrinkapp/ProGuardRunnerTest.java
+++ jshrink/jshrink-app/src/test/java/edu/ucla/cs/jshrinkapp/ProGuardRunnerTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProGuardRunnerTest {
     @Test
@@ -13,5 +17,47 @@ public class ProGuardRunnerTest {
         String dependenciesPath = ProGuardRunner.class.getClassLoader().getResource("proguard" + File.separator + "hamcrest-core-1.3.jar").getPath();
         ProGuardRunner runner = new ProGuardRunner();
         runner.run(jarFilePath, dependenciesPath);
+    }
+
+    @Test
+    public void testBundleClassFiles() throws IOException, InterruptedException {
+        String classFilePath = ProGuardRunnerTest.class.getClassLoader().getResource("lambda-test-project" + File.separator + "target" + File.separator + "classes").getPath();
+        ArrayList<String> classFiles = new ArrayList<String>();
+        classFiles.add("Main.class");
+        classFiles.add("NumericTest.class");
+        classFiles.add("StandardStuff.class");
+        ProGuardRunner runner = new ProGuardRunner();
+        File f = new File("test.jar");
+        assertFalse(f.exists());
+        boolean exitCode = runner.bundleClassFiles(classFilePath, classFiles, f.getAbsolutePath(), false);
+        assertTrue(f.exists());
+        f.deleteOnExit();
+        assertTrue(exitCode);
+    }
+
+    @Test
+    public void testBundleFolder() throws IOException, InterruptedException {
+        String classFilePath = ProGuardRunnerTest.class.getClassLoader().getResource("lambda-test-project" + File.separator + "target").getPath();
+        ArrayList<String> classFiles = new ArrayList<String>();
+        classFiles.add("classes");
+        ProGuardRunner runner = new ProGuardRunner();
+        File f = new File("test.jar");
+        assertFalse(f.exists());
+        boolean exitCode = runner.bundleClassFiles(classFilePath, classFiles, f.getAbsolutePath(), false);
+        assertTrue(f.exists());
+        f.deleteOnExit();
+        assertTrue(exitCode);
+    }
+
+    @Test
+    public void testBundleSingleModuleMavenProject() throws IOException, InterruptedException {
+        String projectDir = ProGuardRunnerTest.class.getClassLoader().getResource("lambda-test-project").getPath();
+        ProGuardRunner runner = new ProGuardRunner();
+        File f = new File("test.jar");
+        assertFalse(f.exists());
+        boolean exitCode = runner.bundleMavenProject(projectDir, f.getAbsolutePath());
+        assertTrue(f.exists());
+        f.deleteOnExit();
+        assertTrue(exitCode);
     }
 }
